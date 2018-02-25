bb95146e1e981e46393e448a1117b611246c257f
Jarry <runfengai@163.com>
2018-02-25 22:24:09 +0800

    Initial commit

---
+++ app/src/main/java/com/jarry/jchat/App.java
@@ -0,0 +1,35 @@
+package com.jarry.jchat;
+
+import android.app.Activity;
+import android.app.Application;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Jarry on 2018/2/25.
+ */
+
+public class App extends Application {
+    private static List<Activity> list = new ArrayList<>();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    public static void addAct(Activity act) {
+        list.add(act);
+    }
+
+    public static void removeAct(Activity act) {
+        list.remove(act);
+    }
+
+    public void exit() {
+        for (Activity act : list) {
+            act.finish();
+        }
+    }
+}
---
+++ app/src/main/java/com/jarry/jchat/ui/WelcomeActivity.java
@@ -0,0 +1,48 @@
+package com.jarry.jchat.ui;
+
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+import com.eftimoff.androipathview.PathView;
+import com.jarry.jchat.R;
+import com.jarry.jchat.databinding.ActivityWelcomeBinding;
+import com.jarry.jchat.model.Welcome;
+
+public class WelcomeActivity extends BaseActivity {
+    private ActivityWelcomeBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_welcome);
+        Welcome welcome = new Welcome("JChat");
+        binding.setWel(welcome);
+        final PathView pathView = (PathView) findViewById(R.id.pathView);
+
+        pathView.getPathAnimator().
+                delay(100).
+                duration(1500).
+                interpolator(new AccelerateDecelerateInterpolator()).
+                start();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(self, LoginActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+    }
+}
